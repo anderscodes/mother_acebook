22e25ab58eefba08c38780b94661f19f81566143
Igor Ryabchuk <riabtchuk@gmail.com>
2018-06-08 12:43:38 +0100

    db  is connected

--- src/main/java/com/makersacademy/acebook/model/Post.java
+++ src/main/java/com/makersacademy/acebook/model/Post.java
@@ -1,22 +1,28 @@
 package com.makersacademy.acebook.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "POSTS")
 public class Post {
 
-    private @Id @GeneratedValue Long id;
+    private @Id @GeneratedValue(strategy = GenerationType.AUTO) Long id;
+
+    @Column(name = "content")
     private String content;
 
     private Post() {}
 
     public Post(String content) {
         this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Content is %s", content);
     }
 
 }
