7e5b729c764adeeaaedefc3e707a1878c8e0b7c0
anderscodes <anders.j.persson@hotmail.com>
2018-06-11 16:18:12 +0100

    create dialog component created

--- src/main/js/create/createDialog.js
+++ src/main/js/create/createDialog.js
@@ -4,14 +4,26 @@ class CreateDialog extends React.Component {
     constructor(props) {
         super(props);
         this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('a comment was submitted ' + this.state.value);
+        // event.preventDefault();
     }
 
     render() {
         return(
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <label>
                     Post:
-                    <input type="text" value={this.state.value}/>
+                    <input type="text" value={this.state.value} onChange={this.handleChange}/>
                 </label>
                 <input type="submit" value="Submit"/>
             </form>
