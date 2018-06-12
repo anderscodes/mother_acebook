e4a502d6fc01aa0c8b5a16047259bf244c7da1e8
Charly Mannion <charlottemannion100@gmail.com>
2018-06-12 12:26:38 +0100

    fiddling with jest tests

---
+++ src/main/js/test/createDialog.test.js
@@ -0,0 +1,10 @@
+//import React from 'react';
+//import CreateDialog from './createDialog';
+//import renderer from 'react-test-renderer';
+//
+//describe('CreateDialog', () => {
+//    it('renders without crashing', () => {
+//        const div = document.createElement('div');
+//        ReactDOM.render(<CreateDialog/>, div);
+//    });
+//});
---
+++ src/main/js/test/post-test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Posts from '../posts/posts';
+import { render } from "react-testing-library";
+import "dom-testing-library/extend-expect";
+
+describe('Display posts', funtion (){
+    const {getByTestId} = render(
+        <Posts />
+    );
+
+    it('displays a title,', function() {
+        expect(getByTestId("posts-title")).toHaveTextContent("Posts")
+    })
+})
