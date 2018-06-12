f666ed6cba3dddc1fa5e3e3f2668c13e184b5581
Igor Ryabchuk <riabtchuk@gmail.com>
2018-06-12 15:52:55 +0100

    working out automatic reload of the page following the creation of the new post. logic moved to wrapper

--- src/main/js/posts/createPost.js
+++ src/main/js/posts/createPost.js
@@ -4,42 +4,15 @@ const client = require('../client');
 class CreatePost extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.onCreate = this.onCreate.bind(this)
     }
 
-    handleChange(event) {
-        this.setState({value: event.target.value});
-    }
-
-    handleSubmit(event) {
-        event.preventDefault();
-        var newPost = {}
-        newPost["content"] = this.state.value
-        this.onCreate(newPost)
-
-    }
-
-    onCreate(newPost) {
-        client({method: 'POST',
-            path: '/api/posts',
-            entity: newPost,
-            headers: {'Content-Type': 'application/json'}
-        }).done(); console.log("Hi")
-        //     .then(response => {client({method: 'GET', path: '/api/posts'}).then(response => {
-        //     this.setState({posts: response.entity._embedded.posts});
-        // })})
-    }
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
+            <form onSubmit={this.props.handleSubmit}>
                 <label>
-                    Name:
-                    <input type="text" name="content" value={this.state.value} onChange={this.handleChange} />
+                    Create New Post:
+                    <input type="text" name="content" value={this.props.value} onChange={this.props.handleChange} />
                 </label>
                 <input type="submit" value="Submit" />
             </form>
--- src/main/js/wrapper/logicWrapper.js
+++ src/main/js/wrapper/logicWrapper.js
@@ -6,7 +6,34 @@ import CreatePost from  '../posts/createPost';
 class LogicWrapper extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {posts: []};
+    this.state = {
+      posts: [],
+      value: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.onCreate = this.onCreate.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    var newPost = {}
+    newPost["content"] = this.state.value
+    this.onCreate(newPost)
+
+  }
+
+  onCreate(newPost) {
+    client({method: 'POST',
+      path: '/api/posts',
+      entity: newPost,
+      headers: {'Content-Type': 'application/json'}
+    }).then(response=> {this.setState({posts: response.entity._embedded.posts})
+    })
   }
 
     componentDidMount() {
@@ -19,7 +46,7 @@ class LogicWrapper extends React.Component {
     return (
     <div>
       <div>
-        <CreatePost />
+        <CreatePost value={this.state.value} handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>
       </div>
       <div>
         <Posts posts={this.state.posts}/>
