8ce503ebf236d2e5c67909649cf13b208b73ec25
Charly Mannion <charlottemannion100@gmail.com>
2018-06-15 13:05:35 +0100

    user logs in - potential login bug identified

---
+++ src/test/feature/UserLogin.js
@@ -0,0 +1,16 @@
+module.exports = {
+  'user fills in username on login form' : function (browser) {
+    browser
+      .url("http://motherbook.herokuapp.com")
+      .waitForElementVisible('h1', 2000)
+      .assert.containsText('h1', 'Welcome to Acebook')
+      .waitForElementVisible('#app > div > div > div:nth-child(4) > button', 2000)
+      .click('#app > div > div > div:nth-child(4) > button')
+      .pause(1000)
+      .assert.containsText('label', 'Login:Username')
+      .setValue('#app > div > form > label > label > input[type="text"]', 'EllieK')
+      .click('#app > div > form > label > input[type="submit"]')
+//      .assert.containsText('h1', 'Welcome,')
+      .end()
+  }
+}
