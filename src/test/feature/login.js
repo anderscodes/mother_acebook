f555fe2590f20be9788c172e5d494f0c16b4bd01
Charly Mannion <charlottemannion100@gmail.com>
2018-06-16 10:36:11 +0100

    user clicks login feature test passing

---
+++ src/test/feature/login.js
@@ -0,0 +1,13 @@
+ module.exports = {
+   'click on login button' : function (browser) {
+     browser
+       .url("http://motherbook.herokuapp.com")
+       .waitForElementVisible('h1', 2000)
+       .assert.containsText('h1', 'Welcome to Acebook')
+       .waitForElementVisible('#app > div > div > div:nth-child(4) > button', 2000)
+       .click('#app > div > div > div:nth-child(4) > button')
+       .pause(1000)
+       .assert.containsText('label', 'Login:Username')
+       .end()
+   }
+ }
